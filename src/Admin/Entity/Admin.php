1dcdebd839bfdf5f5ee1936b9b41d2f3b3696553
hoheart <hoheart@163.com>
2015-04-27 22:33:53 +0800

    add login js

---
+++ src/Admin/Entity/Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Admin\Entity;
+
+use User\Entity\User;
+
+/**
+ * 数据类：管理员
+ * @hhp:orm entity
+ * @hhp:orm saveName Admin
+ *
+ * @author Jejim
+ *        
+ */
+class Admin extends User {
+}
